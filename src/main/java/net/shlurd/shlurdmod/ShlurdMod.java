3b8de38fc1c0d41a57f4e0eaab42bc3ebf672088
Szilard <szilardigaz@protonmail.com>
2024-11-11 16:35:21 +0000

    Starting

---
+++ src/main/java/net/shlurd/shlurdmod/ShlurdMod.java
@@ -0,0 +1,17 @@
+package net.shlurd.shlurdmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShlurdMod implements ModInitializer {
+	public static final String MOD_ID = "shlurdmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/shlurd/shlurdmod/ShlurdModClient.java
@@ -0,0 +1,10 @@
+package net.shlurd.shlurdmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShlurdModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
